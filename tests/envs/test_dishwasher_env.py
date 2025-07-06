f38d42271fdbc1203bbae2a05df16a4350236fe9
Cybaster <1219925930@qq.com>
2025-07-06 13:32:46 +0800

    complete dishwasher environment

--- tests/envs/test_dishwasher_env.py
+++ tests/envs/test_dishwasher_env.py
@@ -1,19 +1,5 @@
 from imitation_learning_lerobot.envs import DishWasherEnv
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 if __name__ == '__main__':
     env = DishWasherEnv(render_mode="human")
-    data = env.run()
-    actions = np.array(data["actions"])
-    plt.figure(1)
-    for i in range(7):
-        plt.plot(actions[:, i], label=str(i))
-    plt.legend()
-
-    plt.figure(2)
-    for i in range(7):
-        plt.plot(actions[:, 7 + i], label=str(i))
-    plt.legend()
-    plt.show()
+    env.run()
